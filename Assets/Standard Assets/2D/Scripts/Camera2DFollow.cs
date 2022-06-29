5f6a1ea487f5c12e5bac3a2cce8273b6f81a1670
Dario <dfurda18@gmail.com>
2022-06-29 16:03:06 -0300

    Level setup
    I've created a level setip and added the Assets from the standard assets

--- Assets/Standard Assets/2D/Scripts/Camera2DFollow.cs
+++ Assets/Standard Assets/2D/Scripts/Camera2DFollow.cs
@@ -1,17 +1,33 @@
 using System;
 using UnityEngine;
+using System.Collections.Generic;
+
+[Serializable]
+public class CameraBoundries
+{
+    public float top;
+    public float bottom;
+    public float left;
+    public float right;
+}
 
 namespace UnityStandardAssets._2D
 {
     public class Camera2DFollow : MonoBehaviour
     {
+        [SerializeField]
+        public CameraBoundries[] limits;
         public Transform target;
         public float damping = 1;
         public float lookAheadFactor = 3;
         public float lookAheadReturnSpeed = 0.5f;
         public float lookAheadMoveThreshold = 0.1f;
 
+        
+
+
         private float m_OffsetZ;
+        private Vector3 m_OffsetY;
         private Vector3 m_LastTargetPosition;
         private Vector3 m_CurrentVelocity;
         private Vector3 m_LookAheadPos;
@@ -19,8 +35,8 @@ namespace UnityStandardAssets._2D
         // Use this for initialization
         private void Start()
         {
-            m_LastTargetPosition = target.position;
             m_OffsetZ = (transform.position - target.position).z;
+            m_LastTargetPosition = target.position + m_OffsetY;
             transform.parent = null;
         }
 
@@ -41,13 +57,31 @@ namespace UnityStandardAssets._2D
             {
                 m_LookAheadPos = Vector3.MoveTowards(m_LookAheadPos, Vector3.zero, Time.deltaTime*lookAheadReturnSpeed);
             }
-
+            
             Vector3 aheadTargetPos = target.position + m_LookAheadPos + Vector3.forward*m_OffsetZ;
             Vector3 newPos = Vector3.SmoothDamp(transform.position, aheadTargetPos, ref m_CurrentVelocity, damping);
 
-            transform.position = newPos;
+            transform.position = new Vector3(
+                    Mathf.Clamp(newPos.x, limits[0].left, limits[0].right),
+                    Mathf.Clamp(newPos.y, limits[0].bottom, limits[0].top),
+                    newPos.z
+                );
+
+            //transform.position = newPos;
 
             m_LastTargetPosition = target.position;
+        }
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.red;
+            for(int counter = 0; counter < limits.Length; counter++)
+            {
+                Gizmos.DrawLine(new Vector2(limits[counter].left, limits[counter].top), new Vector2(limits[counter].right, limits[counter].top));
+                Gizmos.DrawLine(new Vector2(limits[counter].left, limits[counter].top), new Vector2(limits[counter].left, limits[counter].bottom));
+                Gizmos.DrawLine(new Vector2(limits[counter].right, limits[counter].bottom), new Vector2(limits[counter].left, limits[counter].bottom));
+                Gizmos.DrawLine(new Vector2(limits[counter].right, limits[counter].top), new Vector2(limits[counter].right, limits[counter].bottom));
+            }
+            
         }
     }
 }
